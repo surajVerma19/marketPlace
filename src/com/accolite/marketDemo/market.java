3daf975d12a5d6df8b5565d127b9f7fbfe918a02
Suraj Verma <suraj.verma@accoliteindia.com>
2018-01-15 03:17:41 +0530

    working on market

--- src/com/accolite/marketDemo/market.java
+++ src/com/accolite/marketDemo/market.java
@@ -1,10 +1,62 @@
 package com.accolite.marketDemo;
 
-public class market {
+import java.util.Queue;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Market {
+	static int[] market;
+	String fruitName;
+	static fruitCount;
+	static Queue<Integer> farmerQ;
+	
+	public static void main(String[] args) 
+	{
+		
+				
 
 	}
+	public static void getFruit(int fruit)
+	{
+		checkWaitingFarmer();
+		if(market[fruit] < 1)
+		{
+			market[fruit]--;
+			fruitCount--;
+			System.out.println("consumer waiting for " + fruitName[fruit]);
+		}
+		else if(market[fruit] == 1)
+		{
+			market[fruit]--;
+			fruitCount--;
+			System.out.println("consumer buy " + fruitName[fruit]);
+		}
+		else
+			System.out.println("Some ERROR");
+	}
+	public static void sell(int fruit)
+	{
+		if(fruitCount>5)
+		{
+			farmerQ.add(fruit);
+			System.out.println('farmer waiting to sell' + fruitName[fruit]);
+		}
+		else
+		{
+			fruitCount++;
+			market[fruit]++;
+			System.out.println('farmer place fruit' + fruitName[fruit]);
+		}		
+	}
+	private static checkWaitingFarmer()
+	{
+		if(fruitCount<5)
+		{
+			int fruit = farmerQ.remove();
+			fruitCount++;
+			market[fruit]++;
+			System.out.println('farmer place fruit from waiting queue' + fruitName[fruit]);
+		}
+	}
+	
+	
 
 }
